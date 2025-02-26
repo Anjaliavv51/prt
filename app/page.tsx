ea6be5527e5c78c0f892735ac50b380dd4806653
ASmashCoder <plpanjali05@gmail.com>
2025-02-26 16:57:53 +0530

    Done
    #2

--- app/page.tsx
+++ app/page.tsx
@@ -3,13 +3,14 @@ import { FloatingNav } from "@/components/ui/floating-navbar";
 import BentoGrid from "@/components/Grid";
 import { navItems } from "@/data";
 //import CanvasCursor from "@/components/cursor";
-
+import RecentProjects from "@/components/RecentProjects";
+import Experience from "@/components/Experience";
+import Approach from "@/components/Approach";
 
 export default function Home() {
   return (
     <main className="relative bg-black-100 flex justify-center items-center
-    flex-col overflow-hidden
-    mx-auto sm:px-10 px-5" suppressHydrationWarning>
+    flex-col overflow-hidden mx-auto sm:px-10 px-5">
       {/*<CanvasCursor/>*/}
       <div className="max-w-7xl w-full">
         <FloatingNav
@@ -17,6 +18,9 @@ export default function Home() {
         />
         <Hero/>
         <BentoGrid/>
+        <RecentProjects/>
+        <Experience/>
+        <Approach/>
       </div>
     </main>
   );
